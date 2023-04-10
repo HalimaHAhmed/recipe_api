83ad337b0200483ee0a23f74b6c7da7e2f46b768
HalimaHAhmed <hanahussein6155@gmail.com>
2023-04-10 14:53:44 +0300

    view users updated

--- app/user/views.py
+++ app/user/views.py
@@ -5,13 +5,13 @@ views for the user api
 """
 
 # view part
-from rest_framework import generics, authentication,permissions
+from rest_framework import generics, authentication, permissions
 
-from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
 from user.serializers import (
-    UserSerializer,AuthTokenSerializer,
+    UserSerializer, AuthTokenSerializer,
 )
 
 
@@ -23,28 +23,25 @@ class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
 
-
-class CreateTokenView(obtain_auth_token):
+class CreateTokenView(ObtainAuthToken):
     """
     create a new auth token for a user
     """
 
-    serializer_class =AuthTokenSerializer
-    renderer_classes =api_settings.DEFAULT_RENDERER_CLASSES
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
-    def ManageUserView(generics,RetrieveUpdateApiView):
-        """
-        manage the authenticated user
-        """
 
-        serializer_class = UserSerializer
-        authentication_classes = [authentication.TokenAuthentication]
+class ManageUserView(generics.RetrieveUpdateAPIView):
 
-        permission_classes = [permissions.IsAuthenticated]
+    """
+    manage the authenticated user
+    """
+
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
-        """
-        retrieve and return the authenticated user
-        """
-
         return self.request.user
