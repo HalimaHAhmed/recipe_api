f4572d3d820ad1142ae1ca08ad7c98a46bf8fdde
HalimaHAhmed <hanahussein6155@gmail.com>
2023-04-11 12:31:16 +0300

    commit

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,5 +19,6 @@ urlpatterns = [
             url_name='api-schema'), name='api-docs'
     ),
 
-    path('api/user/', include('user.urls'))
+    path('api/user/', include('user.urls')),
+    path('api/recipe/',include('recipe.urls')),
 ]
